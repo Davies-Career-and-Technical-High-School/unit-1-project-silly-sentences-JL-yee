5485fcb52ea689d06fa62fe71e3db7a2cb87ef4e
Jean  Preira <jeanmarie.preira@daviestech.org>
2020-10-05 13:48:42 +0000

    turn in

---
+++ silly_sentences.py
@@ -0,0 +1,20 @@
+print ("Let's play Silly Sentances")
+
+n = input('Enter a name: ')
+a1 = input('Enter a adjective: ')
+a2 = input('Enter a adjective: ')
+ad = input('Enter a adverb: ')
+f1 = input('Enter a food: ')
+f2 = input('Enter another food: ')
+n2 = input('Enter a noun: ')
+p = input('Enter a place: ')
+v = input('Enter a verb: ')
+
+print(n + " was planning a dream vacation to  . " + n + " was especially looking forward to try a local food place including " + f1  +  f2
+  + ad + " " + a2 + ".")
+
+print("At the " + n + ", " + a1 + " had " + f2 + " at the time.")
+
+print(n + " will have to practice the language " + ad + " to make it easier to " + v + " with people. ")
+
+print(n + " has a long list of sights to see, including the " + n2 + " museum and the " + a2 + "park.")
